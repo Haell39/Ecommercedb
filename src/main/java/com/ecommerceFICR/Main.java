d796011f31009aa96f2b59e82cdc60f8801ae78a
Rael <santosrafaelpro@gmail.com>
2024-12-11 14:56:05 -0300

    updt

--- src/main/java/com/ecommerceFICR/Main.java
+++ src/main/java/com/ecommerceFICR/Main.java
@@ -1,7 +1,17 @@
 package com.ecommerceFICR;
 
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("Teste-unit");
+        EntityManager em = emf.createEntityManager();
+
+        System.out.println("Conexão bem-sucedida!");
+
+        em.close();
+        emf.close();
     }
 }
